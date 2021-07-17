f5d507e1294774d2b267d2a1b789c891c9b88378
Vishal Kumar Shaw <vkshaw1997@gmail.com>
2021-07-17 22:28:54 +0530

    First Commit

---
+++ first.py
@@ -0,0 +1,17 @@
+# ########Chapter - 1 - 
+
+print("Hello World")
+
+# what is pip
+
+# modules -> 1. Built in module
+#2-> External module
+
+# using python as a calculator
+
+# python repl- Read, Evaluate, Print, Loop
+
+
+# comments in python
+# single line comment - #
+# multiline comment - '''    '''
